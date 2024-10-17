f3aac88ccc6424e3ccff52d3ad52a9153e8c4bbe
Maggie Hall <maggiehall1063@gmail.com>
2024-10-17 08:27:42 -0400

    attempting to follow lecture.

--- assignment3/bin/assignment3.py
+++ assignment3/bin/assignment3.py
@@ -1,1 +1,9 @@
+import argparse
 import maya.standalone
+maya.standalone.initialize()
+import maya.cmds
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--radius', help =)
+maya.cmds.polySphere()
+maya.cmds.file(rename="C:\Users\maggi\OneDrive - Drexel University\Year3\fallWinter\techDirecting\week3\anim435-2024\standalone_example\mewsphere.mb")
