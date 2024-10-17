b347044ccf3a0db6c65903cceb99c9a9817ecc9b
Maggie Hall <maggiehall1063@gmail.com>
2024-10-17 13:06:50 -0400

    added code to create a maya file. attempting to run it in gitbash

--- assignment3/bin/assignment3.py
+++ assignment3/bin/assignment3.py
@@ -6,7 +6,7 @@ import maya.cmds
 #testing sumbission
 #Created entire new directory
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-r', '--radius', help =)
 maya.cmds.polySphere()
-maya.cmds.file(rename="C:\Users\maggi\OneDrive - Drexel University\Year3\fallWinter\techDirecting\week3\anim435-2024\standalone_example\mewsphere.mb")
+maya.cmds.file(rename=r"C:\Users\maggi\OneDrive - Drexel University\Year3\fallWinter\techDirecting\week3\anim435-2024\standalone_example\newsphere.mb")
+maya.cmds.file(save=True)
+
